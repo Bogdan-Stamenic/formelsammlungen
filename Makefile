58a4dc25b85b03301bf9b42e12f5c3d0094facac
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2020-11-17 16:16:44 +0100

    fixed template title and author.

--- Makefile
+++ Makefile
@@ -4,12 +4,12 @@ all:
 fuw: FORCE
 	make fuw_fs.pdf
 
-template.pdf: template.tex util/* customize/customize_template.tex
+template.pdf: template.tex
 	pdflatex template.tex
 	pdflatex template.tex
 	make clean
 
-fuw_fs.pdf: fuw_fs.tex util/* customize/customize_fuw_fs.tex
+fuw_fs.pdf: fuw_fs.tex
 	pdflatex fuw_fs.tex
 	pdflatex fuw_fs.tex
 	make clean
--- new_fs.sh
+++ new_fs.sh
@@ -19,9 +19,9 @@ fi
 read -t 30 -p "Enter title of Formelsammlung (default: $DEFAULT_TITLE): " TITLE
 if  [ "$TITLE" != "" ]
 then
-	sed -i "s/title_of_formelsammlung/$TITLE/" $FILE
+	sed -i "s/TitleOfFormelsammlung/$TITLE/" $FILE
 else
-	sed -i "s/title_of_formelsammlung/$DEFAULT_TITLE/" $FILE
+	sed -i "s/TitleOfFormelsammlung/$DEFAULT_TITLE/" $FILE
 fi
 
 
@@ -29,9 +29,9 @@ fi
 read -t 30 -p "Enter name of Author (default: $DEFAULT_AUTHOR): " AUTHOR
 if  [ "$AUTHOR" != "" ]
 then
-	sed -i "s/name_of_author/$AUTHOR/" $FILE
+	sed -i "s/NameOfAuthor/$AUTHOR/" $FILE
 else
-	sed -i "s/name_of_author/$DEFAULT_AUTHOR/" $FILE
+	sed -i "s/NameOfAuthor/$DEFAULT_AUTHOR/" $FILE
 fi
 
 unset FILE TITLE AUTHOR DEFAULT_FILE DEFAULT_TITLE DEFAULT_AUTHOR
